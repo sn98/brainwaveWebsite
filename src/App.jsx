9e39c13216907c187bb5f32d7d960c9af8b5fee0
SG_98 <sean@knightfoxapps.com>
2024-09-17 12:08:20 -0500

    Added collaboration section

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
-      <Header />
-      <Hero />
-      <Benefits />
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+      </div>
+
       <ButtonGradient />
     </>
   );
