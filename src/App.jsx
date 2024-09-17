7091375dc33c2d93f71d7e5536f14e47b518cfee
SG_98 <sean@knightfoxapps.com>
2024-09-17 14:00:04 -0500

    added services section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 const App = () => {
   const [count, setCount] = useState(0);
 
@@ -15,6 +16,7 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
 
       <ButtonGradient />
